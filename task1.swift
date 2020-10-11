284f6e44f3a36ed3bbf4890e43290308fc926f64
KabStas <71084585+KabStas@users.noreply.github.com>
2020-10-11 12:04:35 +0700

    Add files via upload

---
+++ task1.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func countingDistance(lengthOfPool: Int, widthOfPool: Int, distanceToLongSide1: Int, distanceToShortSide1: Int) {
+
+	let distanceToLongSide2 = widthOfPool - distanceToLongSide1
+	let distanceToShortSide2 = lengthOfPool - distanceToShortSide1
+	let numbers: [Int] = [distanceToLongSide1, distanceToShortSide1, distanceToLongSide2, distanceToShortSide2]
+			
+	print(numbers.min()!)
+}
+
+if CommandLine.arguments.count == 1 {
+
+	let lengthOfPool = Int(readLine(strippingNewline: true)!)!
+	let widthOfPool = Int(readLine(strippingNewline: true)!)!
+	let distanceToLongSide1 = Int(readLine(strippingNewline: true)!)!
+	let distanceToShortSide1 = Int(readLine(strippingNewline: true)!)!  
+
+    countingDistance(lengthOfPool: lengthOfPool, widthOfPool: widthOfPool, 
+		distanceToLongSide1: distanceToLongSide1, distanceToShortSide1: distanceToShortSide1)
+	
+} else {
+
+	let lengthOfPool = Int(CommandLine.arguments[1])!
+	let widthOfPool = Int(CommandLine.arguments[2])!
+	let distanceToLongSide1 = Int(CommandLine.arguments[3])!
+	let distanceToShortSide1 = Int(CommandLine.arguments[4])! 
+    
+    countingDistance(lengthOfPool: lengthOfPool, widthOfPool: widthOfPool, 
+		distanceToLongSide1: distanceToLongSide1, distanceToShortSide1: distanceToShortSide1)
+}
