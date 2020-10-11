de651706e7d3f7dc68f8162c37cbd0c4e14504a0
KabStas <71084585+KabStas@users.noreply.github.com>
2020-10-11 18:35:03 +0700

    Add files via upload

---
+++ task5.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+func creatingIntersection(set1: Set<Int>, set2: Set<Int>) {
+
+    let commonValuesSet = set1.intersection(set2)
+    let array = commonValuesSet.sorted()	
+			
+	print(array)
+}
+
+if CommandLine.arguments.count == 1 {
+
+	let lengthOfArray1 = Int(readLine()!)!
+	var set1: Set<Int> = []
+
+    for _ in 0...lengthOfArray1 - 1 {
+        set1.insert(Int(readLine()!)!) 
+    }
+
+    let lengthOfArray2 = Int(readLine()!)!
+	var set2: Set<Int> = []
+
+    for _ in 0...lengthOfArray2 - 1 {
+        set2.insert(Int(readLine()!)!) 
+    }
+
+    creatingIntersection(set1: set1, set2: set2)
+	
+} else {
+
+	let quantityOfNumbers1 = Int(CommandLine.arguments[1])!
+	var set1: Set<Int> = []
+    
+    for i in 2...quantityOfNumbers1 + 1 {
+        set1.insert(Int(CommandLine.arguments[i])!) 
+    }
+	let quantityOfNumbers2 = Int(CommandLine.arguments[1])!
+	var set2: Set<Int> = []
+    
+    for i in 2...quantityOfNumbers2 + 1 {
+        set2.insert(Int(CommandLine.arguments[i])!) 
+    }
+
+    creatingIntersection(set1: set1, set2: set2)
+}
